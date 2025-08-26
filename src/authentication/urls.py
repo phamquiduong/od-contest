2a0d72dcd271495b5fbc019a4c615f16703a3555
ngothikimoanh <ntkoanh.orin@outlook.com>
2025-08-26 16:43:40 +0700

    Tạo login view

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from authentication.views import register_view
+from authentication.views import login_view, register_view
 
 urlpatterns = [
-    path('register', register_view, name='auth_register')
+    path('register', register_view, name='auth_register'),
+    path('login', login_view, name='auth_login'),
 ]
