e18a06460a7bfae9149e7052f3cd5ab3c8302eb4
phamquiduong <phamquiduong@outlook.com>
2025-08-30 12:19:45 +0700

    Cập nhật giao diện toàn hệ thống

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from authentication.views import change_password_view, login_view, register_view
@@ -5,5 +6,6 @@ from authentication.views import change_password_view, login_view, register_view
 urlpatterns = [
     path('register', register_view, name='auth_register'),
     path('login', login_view, name='auth_login'),
+    path('logout', LogoutView.as_view(next_page='home'), name='auth_logout'),
     path('change-password', change_password_view, name='auth_change-password')
 ]
