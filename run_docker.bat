ef6b6334f069c80b22aac2b2f6d02962651ef61d
phamquiduong <phamquiduong@outlook.com>
2025-09-03 00:11:31 +0700

    Tạo app gửi mail

--- run_docker.bat
+++ run_docker.bat
@@ -2,11 +2,14 @@
 
 cd docker
 
+@REM Cài đặt biến môi trường
 if not exist ".env" (
     echo Copying .env.example to .env...
     copy .env.example .env
 )
 
+@REM Tắt các docker container cũ
 docker-compose down
 
+@REM Build và bật docker compose dưới nền
 docker-compose up --build -d
--- run_pip_install.bat
+++ run_pip_install.bat
@@ -1,6 +1,10 @@
 @echo off
 
+@REM Nâng cấp pip
 python -m pip install --upgrade pip
 
+@REM Cài đặt requirements chính
 pip install -r requirements.txt
+
+@REM Cài đặt requirements cho môi trường dev
 pip install -r requirements.dev.txt
